ec5386418cfb950d44cad551cb56519afa461300
nms377 <nms377@nyu.edu>
2017-03-21 22:53:34 -1000

    Updated my card modles to include the createdBy and assignedTo columns.

--- models/card.js
+++ models/card.js
@@ -3,7 +3,9 @@ module.exports = function(sequelize, DataTypes) {
   var Card = sequelize.define('Card', {
     title: DataTypes.STRING,
     priority: DataTypes.STRING,
-    status: DataTypes.STRING
+    status: DataTypes.STRING,
+    createdBy: DataTypes.STRING,
+    assignedTo: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
