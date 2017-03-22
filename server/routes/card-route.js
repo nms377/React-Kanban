3630bcd564c3422fe2530a3d590c21f8248973b8
nms377 <nms377@nyu.edu>
2017-03-21 23:44:10 -1000

    Ran my first sanity test using my GET request. I had to unassociate the Card and User models in order to get this to work because the server was lookng for a userId that did not exist.

--- server/routes/card-route.js
+++ server/routes/card-route.js
@@ -1,7 +1,23 @@
 const server = require('../server');
 const express = require('express');
-const {User, Card} = require('../models');
+const db = require('../models');
+const { Card } = db;
 
 const app = express();
 const router = express.Router();
 
+
+
+router.route('/')
+	.get((req, res) => {
+		Card.findAll()
+			.then((cards => {
+				res.send('sanity');
+			}))
+			.catch((err) => {
+				console.log('err', err);
+				res.send('error', err);
+			});
+})
+
+module.exports = router;
