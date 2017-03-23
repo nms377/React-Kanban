b4aa01b80741c6225a5e4bfee6fa55f4665d108f
nms377 <nms377@nyu.edu>
2017-03-22 21:08:34 -1000

    Added a lifecycle to mount my XHR request. Console logged my XHR Request to make sure it loaded.

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
 import MainBoard from '../../components/MainBoard.js';
+import InProgress from '../../components/InProgress.js';
 import './styles.css';
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      mainTitle: 'React Kanban'
+      mainTitle: 'React Kanban',
+      list:
+        {
+          title: 'sanity',
+          priority: 'high',
+          status: 'queue'
+        }
     }
   }
+
+ componentWillMount() {
+
+    function reqListener(){
+      console.log('what is this? ', this.responseText)
+    }
+   var oReq = new XMLHttpRequest();
+   oReq.addEventListener("load", reqListener);
+   oReq.open("GET", "/api/board");
+   oReq.send();
+ }
 
   render() {
     return (
       <div className="App">
         <div className="Main-header">
-         <MainBoard title={this.state.mainTitle}/>
+         <MainBoard
+          mainTitle={this.state.mainTitle}
+         />
+         <InProgress
+           title={this.state.list.title}
+           priority={this.state.list.priority}
+           status={this.state.list.status}         
+         />
         </div>
       </div>
     );
