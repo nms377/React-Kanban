74da985e67c3b7aecacc8eaa3cb82dbece670766
nms377 <nms377@nyu.edu>
2017-03-22 21:07:51 -1000

    Moved my other properties and left the title of the MainBoard.

--- client/src/components/MainBoard.js
+++ client/src/components/MainBoard.js
@@ -3,7 +3,10 @@ import React from 'react';
 const MainBoard = (props) => (
 
 	<div className="Main-header">
-		<h2>{props.title}</h2>
+		<h2>{props.mainTitle}</h2>
+		<p> { props.title }</p>
+		<p> { props.priority }</p>
+		<p> { props.status }</p>
 	</div>
 
 );
