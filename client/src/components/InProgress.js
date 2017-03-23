fcfd795c49cb5fd316f416e070c22546cebbf63b
nms377 <nms377@nyu.edu>
2017-03-22 22:24:37 -1000

    Included created by and assigned to.

--- client/src/components/InProgress.js
+++ client/src/components/InProgress.js
@@ -6,6 +6,8 @@ const InProgress = (props) => (
 		<h2>{props.title}</h2>
 		<p> Priority: {props.priority} </p>
 		<p> Status: {props.status} </p>
+		<p> Created By: {props.createdBy} </p>
+		<p> Assigned To: {props.assignedTo} </p>
 	</div>
 
 );
