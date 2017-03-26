280f3d6b33872741a85b4a7355046d83c76cbb42
nms377 <nms377@nyu.edu>
2017-03-25 20:26:14 -1000

    Steven helped me walk through the implementation of redux props and suggested I move my XHR request into a helper file. Then I mounted my ShowTasks prop in my ComponentWillMount hook to render my data.

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,62 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+//  components
 import MainBoard from '../../components/MainBoard.js';
 import NewCard from '../../components/NewCard.js';
 import InProgress from '../../components/InProgress.js';
 import Queue from '../../components/Queue.js';
 import Done from '../../components/Done.js';
+
+//  misc
 import './styles.css';
+import getCardsReq from '../../lib';
+
+//  actions
+import { showTasks } from '../../actions';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.title ='React Kanban'
-    this.state = {
-      cards: [],
-      title: "",
-      priority: "",
-      status: ""
-    }
-
-    this.addNewCard = this.addNewCard.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
 
   componentDidMount() {
-
-    var that = this;
-
-    function reqListener(){
-      console.log('what is this? ', this.responseText)
-      var data = JSON.parse(this.responseText);
-      that.setState({
-        cards: data
+   getCardsReq()
+      .then( data => {
+        console.log('data: ', data)
+        data.forEach(cards => {
+          console.log('cards', cards)
+          this.props.onShowTasks(cards.title, cards.priority, cards.status);
+        });
       })
-    }
-    let oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
-    oReq.open("GET", "/api/board");
-    oReq.send();
   }
 
-    addNewCard(){
-      let newReq = new XMLHttpRequest();
-      newReq.open("POST", "/api/board")
-      newReq.setRequestHeader("Content-Type", "application/json");
-      newReq.send(JSON.stringify(this.responseText))
-    }
+    // addNewCard(){
+    //   let newReq = new XMLHttpRequest();
+    //   newReq.open("POST", "/api/board")
+    //   newReq.setRequestHeader("Content-Type", "application/json");
+    //   newReq.send(JSON.stringify(this.responseText))
+    // }
 
-    handleSubmit(event){
-      event.preventDefault();
+    // handleSubmit(event){
+    //   event.preventDefault();
   
-      this.addNewCard({
-        title: this.state.title,
-        priority: this.state.priority,
-        status: this.state.status
-      })
+    //   this.addNewCard({
+    //     title: this.state.title,
+    //     priority: this.state.priority,
+    //     status: this.state.status
+    //   })
 
-    }
+    // }
 
   render() {
+    console.log('props', this.props)
     return (
       <div className="App">
         <div className="Main-header">
@@ -70,7 +66,7 @@ class App extends Component {
         <div className="InProgress-header">
           <h1>In Progress</h1>
           { 
-            this.state.cards.filter(({status}) => status === 'in progress').map( ( { title, priority, status, createdBy, assignedTo }) =>
+            this.props.cards.filter(({status}) => status === 'in progress').map( ( { title, priority, status, createdBy, assignedTo }) =>
               <InProgress
                 key={title}
                 title={title}
@@ -86,7 +82,7 @@ class App extends Component {
         <div className="Queue-header">
           <h1>Queue</h1>
           {
-            this.state.cards.filter(({status}) => status === 'queue').map( ( {title, priority, status, createdBy, assignedTo}) => 
+            this.props.cards.filter(({status}) => status === 'queue').map( ( {title, priority, status, createdBy, assignedTo}) => 
               <Queue
                 key={title}
                 title={title}
@@ -102,7 +98,7 @@ class App extends Component {
         <div className="Done-header">
           <h1>Done</h1>
           {
-            this.state.cards.filter(({status}) => status === 'done').map(({title, priority, status, createdBy, assignedTo}) =>
+            this.props.cards.filter(({status}) => status === 'done').map(({title, priority, status, createdBy, assignedTo}) =>
                 <Done
                   key={title}
                   title={title}
@@ -120,4 +116,21 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    cards: state.cards
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onShowTasks: (title, priority, status, createdBy, assignedTo) => {
+      dispatch(showTasks(title, priority, status, createdBy, assignedTo));
+    }
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
