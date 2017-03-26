9ebf37635b5970bf6d63af26283d64be4496df6c
nms377 <nms377@nyu.edu>
2017-03-25 20:22:16 -1000

    Imported createdDevTools, and console logged store so I can see if my data was coming through from the server.

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
-import './index.css';
-
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { createdDevTools } from 'redux-devtools';
+import App from './containers/App';
+import './index.css';
 import cards from './reducers';
+import {getCardsReq} from './lib';
 
 let store = createStore(cards);
 
+	console.log('store', store)
+
+
 ReactDOM.render(
 		<Provider store={store}>
-	  	<App />,
+	  	<App />
 		</Provider>,
   document.getElementById('root')
 );
