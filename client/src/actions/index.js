2cbc7cd9a6963f638b7a147fe282d5c95f47f1e7
nms377 <nms377@nyu.edu>
2017-03-25 20:23:44 -1000

    Updated ADD_TASK to SHOW_TASK because it was breaking my getCardsReq function and my ShowTasks props.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,11 +2,11 @@ export const SHOW_TASK = 'SHOW_TASK'
 
 export function showTasks(title, priority, status, createdBy, assignedTo) {
 	return {
-		type: SHOW_TASK,
+		type: 'SHOW_TASK',
 		title,
 		priority,
 		status,
 		createdBy,
 		assignedTo
-	}
-}
+	};
+};
