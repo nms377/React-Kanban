f99848546c007492d354f5404ff42fd9eb7b0e34
nms377 <nms377@nyu.edu>
2017-03-25 16:09:48 -1000

    Created a reducer for my SHOW_TASK action.

---
+++ client/src/reducers/index.js
@@ -0,0 +1,27 @@
+import { SHOW_TASK } from '../actions'
+
+const initialState = {
+	cards: []
+};
+
+function cards( state = initialState, action) {
+	switch(action.type) {
+		case SHOW_TASK:
+			return Object.assign({}, state, {
+				cards: [
+					...state.cards,
+					{
+						title: action.title,
+						priority: action.priority,
+						status: action.status,
+						createdBy: action.createdBy,
+						assignedTo: action.assignedTo
+					}
+				]
+			})
+		default:
+			return state;
+	}
+}
+
+export default cards;
