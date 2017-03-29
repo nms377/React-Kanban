d2262fc1381cf9c2299bb3ee58d34753f5a43b88
nms377 <nms377@nyu.edu>
2017-03-29 08:07:15 -1000

    Moved my updateTask function to my Card.js file so I can directly hit something. My card updates, but I get a synta error in my XHR request regarding the JSON. Not sure why.

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,15 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateTask } from '../actions';
+import updateCardReq from '../lib/updateTask.js';
 
-const Card = (props) => (
 
-	<div className="Cards">
-		<p>{props.title}</p>
-		<p>Priority: {props.priority}</p>
-		<p>Status: {props.status}</p>
-		<p>Created By: {props.createdBy}</p>
-		<p>Assign To: {props.assignedTo}</p>
-	</div>
+class Card extends Component {
 
-);
+	constructor(props) {
+		super(props);
 
-export default Card;
+		this.handleStatus = this.handleStatus.bind(this);
+	}
+
+	handleStatus(event){
+		event.preventDefault();
+		this.updateTask({
+			title: this.props.title,
+			status: event.target.value,
+			priority: this.props.priority,
+			assignedTo: this.props.assignedTo
+		})
+	}
+
+  updateTask(card){
+    updateCardReq(card)
+      .then(card => {
+      	console.log('WHAT UPDATED:', card)
+        this.props.onUpdateTask(card.id, card.status)          
+      })
+  }
+
+	render(){
+		return(
+			<div className="Cards">
+				<p>{this.props.title}</p>
+				<p>Priority: {this.props.priority}</p>
+				<label>
+					Status:
+					<select defaultValue={this.props.status} onChange={this.handleStatus}>
+						<option value="queue">Queue</option>
+						<option value="in progress">In Progress</option>
+						<option value="done">Done</option>
+					</select>
+				</label>
+				<p>Status: {this.props.status}</p>
+				<p>Created By: {this.props.createdBy}</p>
+				<p>Assign To: {this.props.assignedTo}</p>
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = (state) => {
+  return {
+    cards: state.cards
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onUpdateTask: (id, status) => {
+      dispatch(updateTask(id, status));
+    }
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Card);
