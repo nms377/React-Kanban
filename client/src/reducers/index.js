6bf73616b18ff6d59a2f27ccc9d4f5e509b6499b
nms377 <nms377@nyu.edu>
2017-03-29 01:25:07 -1000

    Refactored my reducer because I wasn't sure why my POST request wasn't working and I thought maybe the reducer was the reason.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,11 @@
-import { ADD_TASK } from '../actions'
+import { ADD_TASK, UPDATE_TASK } from '../actions'
 
 const initialState = {
 	cards: []
 };
 
 function cards( state = initialState, action) {
+	console.log('which action: ', action.type);
 	switch(action.type) {
 		case ADD_TASK:
 			console.log('TASKS WERE DISPLAYED')
@@ -12,6 +13,7 @@ function cards( state = initialState, action) {
 				cards: [
 					...state.cards,
 					{
+						id: action.id,
 						title: action.title,
 						priority: action.priority,
 						status: action.status,
@@ -19,7 +21,24 @@ function cards( state = initialState, action) {
 						assignedTo: action.assignedTo
 					}
 				]
-			})			
+			});
+
+		case UPDATE_TASK:
+			let updatedCard = state.cards.map(card => {
+				if(cards.id === action.id){
+					cards.status = action.status;
+					return cards;
+				}else {
+					return cards
+				}
+			})
+			console.log('TASKS WERE UDPATED');
+			return Object.assign({}, state, {
+				cards: [
+					...updatedCard
+				]
+			})
+
 		default:
 			return state;
 	}
