ffb2a61c8b6901ca536c1efd34c838ca03755e71
nms377 <nms377@nyu.edu>
2017-04-05 12:49:59 -1000

    Thought I got my delete to work properly, however, I received a 400 status error. I stringified my data in my oReq.send and now my server can find the task based on the title, but my client won't reload to remove the task in the browser.

--- client/src/lib/deleteTask.js
+++ client/src/lib/deleteTask.js
@@ -2,7 +2,6 @@ module.exports = function deleteCardReq(card) {
 	return new Promise ( (resolve, reject) => {
 		function reqListener(){
 			console.log('response text in update: ', card);
-			// let card = JSON.parse(this.responseText);
 			console.log('data in update: ', card);
 			resolve(card);
 		}
@@ -11,6 +10,6 @@ module.exports = function deleteCardReq(card) {
 		oReq.addEventListener('progress', reqListener);
 		oReq.open('DELETE', `api/board/delete`);
 		oReq.setRequestHeader("Content-Type", "application/json");
-		oReq.send(card);
+		oReq.send(JSON.stringify(card));
 	});
 };
