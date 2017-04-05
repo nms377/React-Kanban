18b537fd14bfcace688af556874ac80ae7db445b
nms377 <nms377@nyu.edu>
2017-04-05 12:12:00 -1000

    Removed the JSON.stringify from my res.send. Once I removed this my DELETE request worked and the browser would refresh with the new changes. I want to say the reason I don't need the JSON stringify is because I'm not trying to pass new data through but I want to remove existing data and JSON.stringify wouldn't provide all the info I need to do so.

--- client/src/lib/deleteTask.js
+++ client/src/lib/deleteTask.js
@@ -1,16 +1,16 @@
 module.exports = function deleteCardReq(card) {
 	return new Promise ( (resolve, reject) => {
 		function reqListener(){
-			console.log('response text in update: ', this.responseText);
-			let data = JSON.parse(this.responseText);
-			console.log('data in update: ', data);
-			resolve(data);
+			console.log('response text in update: ', card);
+			// let card = JSON.parse(this.responseText);
+			console.log('data in update: ', card);
+			resolve(card);
 		}
 
 		const oReq = new XMLHttpRequest();
-		oReq.addEventListener('load', reqListener);
-		oReq.open('DELETE', `api/board/edit`);
+		oReq.addEventListener('progress', reqListener);
+		oReq.open('DELETE', `api/board/delete`);
 		oReq.setRequestHeader("Content-Type", "application/json");
-		oReq.send(JSON.stringify(card));
+		oReq.send(card);
 	});
 };
