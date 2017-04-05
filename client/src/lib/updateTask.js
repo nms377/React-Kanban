0b4f9203309a2d67f0c6f352e17c93aeb6b2a414
nms377 <nms377@nyu.edu>
2017-04-05 12:10:30 -1000

    Updated my PUT XHR request so my changes could reload in the browser. I also had to update my Card.js file.

--- client/src/lib/updateTask.js
+++ client/src/lib/updateTask.js
@@ -1,10 +1,10 @@
 module.exports = function updateCardReq(card) {
 	return new Promise ( (resolve, reject) => {
 		function reqListener(){
-			console.log('response text in update: ', this.responseText);
-			let data = JSON.parse(this.responseText);
-			console.log('data in update: ', data);
-			resolve(data);
+			console.log('response text in update: ', card);
+			// let data = JSON.parse(this.responseText);
+			console.log('data in update: ', card);
+			resolve(card);
 		}
 
 		const oReq = new XMLHttpRequest();
