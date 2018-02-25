77130463d6f8dcffbd314e7b6b725315106b3843
Nicole Sandry <nicolesandry@Nicoles-MBP.hawaii.rr.com>
2018-02-24 22:01:39 -1000

    Moved the Kanban Title header to App/index.js component. Moved the Queue, In Progress, and Done components into the MainBoard component so it is contained better in the desktop view. Now I have an issue with the height when a card is added. Rather than adjust the column with the most cards it adjusts all columns.

--- client/src/components/MainBoard.js
+++ client/src/components/MainBoard.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const MainBoard = (props) => (
+//  components
+import InProgress from '../components/InProgress.js';
+import Queue from '../components/Queue.js';
+import Done from '../components/Done.js';
 
-	<div className="Main-header">
-		<h1>{props.mainTitle}</h1>
-	</div>
+//  misc
+import getCardsReq from '../lib';
 
-);
+//  actions
+import { addTask, updateTask, deleteTask } from '../actions';
 
-export default MainBoard;
+class MainBoard extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentWillMount() {
+    getCardsReq()
+      .then( data => {
+        console.log('data: ', data)
+        data.forEach(cards => {
+          console.log('cards', cards)
+          this.props.onAddTask(cards.id, cards.title, cards.priority, cards.status, cards.createdBy, cards.assignedTo);
+        });
+      })
+  }
+
+  render() {
+    console.log('props', this.props)
+    return (
+      <div className="MainBoard">
+
+        <Queue cards={this.props.cards} updateTask={this.updateTask} deleteTask={this.deleteTask}/>
+
+        <InProgress cards={this.props.cards} updateTask={this.updateTask} deleteTask={this.deleteTask}/>
+
+        <Done cards={this.props.cards} updateTask={this.updateTask} deleteTask={this.deleteTask}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    cards: state.cards
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAddTask: (id, title, priority, status, createdBy, assignedTo) => {
+      dispatch(addTask(id, title, priority, status, createdBy, assignedTo));
+    },
+    onUpdateTask: (title, status) => {
+      dispatch(updateTask(title, status));
+    },
+    onDeleteTask: (title) => {
+      dispatch(deleteTask(title));
+    }
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainBoard);
