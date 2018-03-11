727a28314a3e5a1053d157794fc52cc09b911320
Nicole Sandry <nms377@nyu.edu>
2018-03-10 21:36:18 -1000

    Created a Login component.

---
+++ client/src/components/Login.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+//  misc
+// import getCardsReq from '../lib';
+
+//  actions
+// import { addTask, updateTask, deleteTask } from '../actions';
+
+class Login extends Component {
+
+  render() {
+    return (
+			
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Username:
+						<input type="text" />
+				</label>
+				<br/>
+				<label>
+					Password:
+						<input type="password" />
+				</label>
+				<br/>
+				<input type="submit" value="Log In"/>
+			</form>
+
+    );
+  }
+}
+
+export default Login;
