adf795ee559982f4c5204666717b6763395adc6d
Nicole Sandry <nms377@nyu.edu>
2018-03-10 21:39:38 -1000

    Updated the App/index so it compiles the User, New User, and Login components instead of the MainBoard component.

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,39 +1,37 @@
 import React, { Component } from 'react';
 
 //  components
-// import Card from '../../components/Card.js';
-import MainBoard from '../../components/MainBoard.js';
+import User from '../User/User.js';
+import MainBoard from '../Board/MainBoard.js';
 import NewCard from '../NewCard/NewCard.js';
 
 //  misc
 import './styles.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      showForm: true
-    }
-    this.title ='React Kanban'
+	constructor(props){
+      super(props);
+      this.state = {
+       showForm: false
+      };
   }
 
   // Toggle to Hide and Show Task Form
   operation(){
     this.setState({
       showForm:!this.state.showForm
-    })
+    });
   }
 
   render() {
     return (
       <div className="App">
         <div className="MainHeader">
-          <h1>{this.title}</h1>
+          <h1>React Kanban</h1>
 
           <div id="addCard" onClick={()=>this.operation()}>
             +
           </div>
-
         </div>
 
       {
@@ -41,8 +39,7 @@ class App extends Component {
           <NewCard />
         :null
       }
-
-        <MainBoard />
+        <User />
       </div>
     );
   }
