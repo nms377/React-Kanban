3520f4d924d775966895bc87ee8a5ff3ae8041fb
Nicole Sandry <nms377@nyu.edu>
2018-03-13 18:50:11 -1000

    Setup props and actions to allow the user to login. If successful, the user is redirected to their profile page.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,33 +1,117 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-
-//  misc
-// import getCardsReq from '../lib';
+import { connect } from 'react-redux';
 
 //  actions
-// import { addTask, updateTask, deleteTask } from '../actions';
+import { addUserToState, userErrMsg } from '../actions/userAction';
 
 class Login extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			username: '',
+			password: ''
+		};
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleUsername = this.handleUsername.bind(this)
+		this.handlePassword = this.handlePassword.bind(this)
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		let username = document.getElementById('username').value;
+		let password = document.getElementById('password').value;
+
+		if (username === '' || password === '') {
+			return this.props.onUserErrMsg('Please enter your email and password');
+		}
+
+		this.userIsLoggedIn({
+			username: this.state.username,
+			password: this.state.password
+		})
+		.then((data) => {
+			if(data) {
+				let userInfo = data;
+				this.props.onSignIn(userInfo.id, userInfo.username);
+				this.props.history.push('/profile');
+			}
+		})
+	}
+
+	handleUsername(event) {
+		this.setState({
+			username: event.target.value
+		});
+	}
+
+	handlePassword(event) {
+		this.setState({
+			password: event.target.value
+		});
+	}
+
+	userIsLoggedIn(user) {
+		return new Promise(function(res,rej) {
+			function reqLIstener(userData) {
+				let results = this.responseText;
+				if (results === null) {
+					rej(results);
+				} else {
+					res(results);
+				}
+			}
+
+			var oReq = new XMLHttpRequest();
+			oReq.addEventListener("load", reqLIstener);
+			oReq.open('POST', 'api/user/login');
+			oReq.setRequestHeader('Content-type', 'application/json');
+			oReq.send(JSON.stringify(user));
+		});
+	}
 
   render() {
     return (
-			
+			<div>
+			<p>{this.props.userErrMsg}</p>
 			<form className="UserInfo" onSubmit={this.handleSubmit}>
 				<label>
 					Username:
-						<input type="text" />
+						<input id="username" type="text" value={this.state.username} onChange={this.handleUsername} />
 				</label>
 				<br/>
 				<label>
 					Password:
-						<input type="password" />
+						<input id="password" type="password" value={this.state.password} onChange={this.handlePassword} />
 				</label>
 				<br/>
 				<input type="submit" value="Log In"/>
 			</form>
-
+			</div>
     );
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+	return {
+		users: state.users
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSignIn: (id, username, loggedIn) => {
+			dispatch(addUserToState(id, username, loggedIn))
+		},
+		onUserErrMsg: (userErrMsg) => {
+			dispatch(userErrMsg(userErrMsg))
+		}
+	}
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Login)
