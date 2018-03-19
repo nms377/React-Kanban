d5eaab371033175842d9af791c03e83540118d64
Nicole Sandry <nms377@nyu.edu>
2018-03-19 11:42:19 -1000

    Updated my model for users to add first_name, last_name, and email to the databse schema.

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,21 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
     username: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        // User.hasMany(models.Card);
+        User.hasMany(models.Card);
       }
     }
   });
