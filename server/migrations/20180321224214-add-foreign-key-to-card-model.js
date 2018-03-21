939581be5d11fe8ced108b61bbbd96f743f78287
Nicole Sandry <nms377@nyu.edu>
2018-03-21 12:59:42 -1000

    Updated Cards models by adding a user column and addding a foreign key to belong to Users models.

---
+++ server/migrations/20180321224214-add-foreign-key-to-card-model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+
+    queryInterface.changeColumn('Cards', 'user',
+    {
+      type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+
+    queryInterface.removeColumn('Cards', 'user',
+    {
+      type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+    });
+  }
+};
