df160b1d92fafae1e967f2749d2efc4be15a8ac5
Nicole Sandry <nms377@nyu.edu>
2018-03-21 17:40:42 -1000

    Added all the migrations I deleted. Then I added new migrations to remove userID column from the card model.

---
+++ server/migrations/20180322003605-remove-userId-from-cards2.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+
+    queryInterface.removeColumn('Cards', 'userId');
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    queryInterface.addColumn('Cards', 'userId', Sequelize.INTEGER);
+  }
+};
